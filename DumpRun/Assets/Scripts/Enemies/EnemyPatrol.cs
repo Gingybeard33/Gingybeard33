893295785ac85ca6910d745903176e9e7e9c490b
Andrew Saake <andrewsaake@rcs.edu>
2022-02-13 23:07:32 -0700

    Simple Enemy AI
    Simple Melee enemy with patrolling implemented.

---
+++ DumpRun/Assets/Scripts/Enemies/EnemyPatrol.cs
@@ -0,0 +1,84 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyPatrol : MonoBehaviour
+{
+    [SerializeField] private Transform leftEdge;
+    [SerializeField] private Transform rightEdge;
+
+    [SerializeField] private Transform enemy;
+
+
+
+    [SerializeField] private float speed;
+    [SerializeField] private float idleDuration;
+    private float idleTimer;
+
+    private Vector3 initScale;
+  
+    public bool movingLeft;
+
+    private void Awake()
+    {
+        initScale = enemy.localScale;
+         
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (movingLeft)
+        {
+            if (enemy.position.x >= leftEdge.position.x)
+            {
+                MoveInDirection(-1);
+            }
+            else
+            {
+                DirectionChange();
+            }
+        }
+        else
+        {
+            if (enemy.position.x <= rightEdge.position.x)
+            {
+                MoveInDirection(1);
+            }
+            else
+            {
+                DirectionChange();
+            }
+        }
+        
+    }
+
+    private void OnDisable()
+    {
+        
+    }
+
+    private void DirectionChange()
+    {
+        idleTimer += Time.deltaTime;
+
+        if (idleTimer > idleDuration)
+        {
+            movingLeft = !movingLeft;
+        }
+
+    }
+
+    private void MoveInDirection(int direction)
+    {
+        idleTimer = 0;
+        enemy.localScale = new Vector3(Mathf.Abs(initScale.x * direction), initScale.y, initScale.z);
+
+        enemy.position = new Vector3(enemy.position.x + Time.deltaTime * direction * speed, enemy.position.y, enemy.position.z);
+    }
+}
---
+++ DumpRun/Assets/Scripts/Enemies/MeleeEnemy.cs
@@ -0,0 +1,95 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MeleeEnemy : MonoBehaviour
+{
+    [SerializeField] private float attackCooldown;
+    [SerializeField] private float range;
+    [SerializeField] private float rangeHeight;
+    [SerializeField] private float colliderDistance;
+    [SerializeField] private int damage;
+    [SerializeField] private BoxCollider2D boxCollider;
+    [SerializeField] private LayerMask playerLayer;
+    private float cooldownTimer = Mathf.Infinity;
+
+    private EnemyPatrol enemyPatrol;
+       
+    
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        cooldownTimer += Time.deltaTime;
+
+        if (PlayerInSightMele())
+        {
+            if (cooldownTimer >= attackCooldown)
+            {
+                cooldownTimer = 0;
+
+            }
+        }
+        if (enemyPatrol != null)
+        {
+            enemyPatrol.enabled = !PlayerInSightMele();
+        }
+    }
+
+    private void Awake()
+    {
+        enemyPatrol = GetComponentInParent<EnemyPatrol>();
+    }
+
+    private bool PlayerInSightMele()
+    {
+        if (!enemyPatrol.movingLeft)
+        {
+            RaycastHit2D hit = Physics2D.BoxCast(boxCollider.bounds.center + transform.right * range * transform.localScale.x * colliderDistance,
+                new Vector3(boxCollider.bounds.size.x * range, boxCollider.bounds.size.y * rangeHeight, boxCollider.bounds.size.z),
+                0, Vector2.left, 0, playerLayer);
+            return hit.collider != null;
+        }
+        else
+        {
+            RaycastHit2D hit = Physics2D.BoxCast(boxCollider.bounds.center + transform.right * range * transform.localScale.x * colliderDistance*-1,
+              new Vector3(boxCollider.bounds.size.x * range, boxCollider.bounds.size.y * rangeHeight, boxCollider.bounds.size.z),
+              0, Vector2.left, 0, playerLayer);
+            return hit.collider != null;
+        }
+
+        
+       
+
+    }
+
+    private void OnDrawGizmos()
+    {
+        Gizmos.color = Color.red;
+        if (!enemyPatrol.movingLeft)
+        {
+            Gizmos.DrawWireCube(boxCollider.bounds.center + transform.right * range * transform.localScale.x * colliderDistance,
+            new Vector3(boxCollider.bounds.size.x * range, boxCollider.bounds.size.y * rangeHeight, boxCollider.bounds.size.z));
+        }
+        else
+        {
+            Gizmos.DrawWireCube(boxCollider.bounds.center + transform.right * range * transform.localScale.x * colliderDistance*-1,
+           new Vector3(boxCollider.bounds.size.x * range, boxCollider.bounds.size.y * rangeHeight, boxCollider.bounds.size.z));
+        }
+    }
+
+    private void DamagePlayer()
+    {
+        if (PlayerInSightMele())
+        {
+            //give damage to player
+        }
+
+    }
+}
